7a191dd18d4de25b4286384793aae30d35d6d223
200456449 <90534140+200456449@users.noreply.github.com>
2021-11-19 19:52:22 -0500

    final

---
+++ LAB5.ps1
@@ -0,0 +1,21 @@
+﻿Param ( [switch]$System, 
+        [switch]$Disks,
+        [switch]$Network
+)
+
+import-module Kamaljotkaur
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+ Write-Host "Insert command line arguments or it will display everything."
+ Write-Host "............................................................"
+ Kamaljotkaur-System;
+ Kamaljotkaur-Disks;
+ Kamaljotkaur-Network;
+} else {
+ if ($System) {
+     Kamaljotkaur-System }
+ if ($Disks) {
+     Kamaljotkaur-Disks; }
+ if ($Network) {
+     Kamaljotkaur-Network }
+}
